92b8fa2cb101f75db0656db5f8192b3cd9a55359
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2017-02-22 13:08:05 -0800

    Initial commit: largely empty web setup

---
+++ development.bat
@@ -0,0 +1,4 @@
+call gulp clean min
+set ASPNETCORE_ENVIRONMENT=Development
+SET ASPNETCORE_URLS=https://*:8045
+dotnet run
---
+++ start.bat
@@ -0,0 +1,4 @@
+call gulp clean min
+set ASPNETCORE_ENVIRONMENT=Production
+SET ASPNETCORE_URLS=https://*:8045
+dotnet run
