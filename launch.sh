b1bc6724aefc81a294e40047750c4fc729ee70ab
mcmonkey4eva <agoodwin@freneticllc.com>
2018-03-18 20:46:56 -0700

    quick fix

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 gulp clean min
 dotnet restore
-ASPNETCORE_ENVIRONMENT=Production ASPNETCORE_URLS=https://*:8045 dotnet run
+ASPNETCORE_ENVIRONMENT=Production ASPNETCORE_URLS=http://*:8045 dotnet run
